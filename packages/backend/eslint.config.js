48a7aca710e93c6466fefacd48f1ec802a00c67d
Alex Truong <46734496+alexhtruong@users.noreply.github.com>
2025-02-05 11:03:53 -0800

    chore: add Prettier and ESLint configurations, remove README, and format code

---
+++ packages/backend/eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+
+export default [
+  { ignores: ["dist"] },
+  {
+    files: ["**/*.{js}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    },
+    rules: {
+      ...js.configs.recommended.rules
+    }
+  }
+];
