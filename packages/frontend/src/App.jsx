7099f4518ada5848475cbc03f8955bb5a7eff580
Sophia Chang <schang7jcu@gmail.com>
2025-02-19 07:38:30 -0800

    added routing

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,11 +1,20 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import "./App.css";
 import Login from "./pages/Login";
-//import SignUp from "./pages/SignUp";
+import SignUp from "./pages/SignUp";
 
 function App() {
     return (
         <>
-        <Login/>
+        <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
         </>
     );
 }
