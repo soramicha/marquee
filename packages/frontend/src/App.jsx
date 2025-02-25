ee1c3d1c75ed8cc80a3fe7e9cc82be7a0a1c992d
James Tran <120448604+jtran273@users.noreply.github.com>
2025-02-24 23:36:44 -0800

    Updated ProfilePage and App.jsx with new design

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,12 +1,18 @@
-//import "./App.css";
-import SignUp from "./pages/SignUp";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";   // Your SignUp page
+import ProfilePage from "./pages/ProfilePage"; // Your Profile Page
 
-function App() {
-    return (
-        <>
-        <SignUp/>
-        </>
-    );
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUp />} />       {/* Default page is SignUp */}
+        <Route path="/signup" element={<SignUp />} /> {/* Signup Page */}
+        <Route path="/profile" element={<ProfilePage />} /> {/* Profile Page */}
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
