d0bc219e7b35a6bc2d8b909de6d537d1a8b99a20
katiehphan <68561300+katiehphan@users.noreply.github.com>
2025-02-26 09:45:44 -0800

    finished navbar

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import "./App.css";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Navbar from "./pages/Navbar";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/">
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<SignUp />} />
+          <Route path="navbar" element={<Navbar/>}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
