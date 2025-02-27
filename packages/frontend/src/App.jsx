bd5cdcf10987a32428286ee60fdf36e95a081eea
Sophia Chang <schang7jcu@gmail.com>
2025-02-27 14:00:09 -0800

    removed unnecessary imports

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import ListingCard from "./components/ui/ListingCard";
-import Login from "@/pages/Login";
-import SignUp from "@/pages/SignUp";
 import PersistLogin from "@/components/PersistLogin";
 import RequireAuth from "@/components/RequireAuth";
 import Home from "@/pages/Home";
