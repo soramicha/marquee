ac3e0bed84e44c3562fec17d39319584d12d969e
Sophia Chang <schang7jcu@gmail.com>
2025-03-11 23:37:04 -0700

    Update vite.config.js

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
@@ -12,4 +13,7 @@ export default defineConfig({
             "@": path.resolve(__dirname, "src"),
         },
     },
+    build: {
+        outDir: "build"  // Change Vite output folder from "dist" to "build"
+    }
 });
