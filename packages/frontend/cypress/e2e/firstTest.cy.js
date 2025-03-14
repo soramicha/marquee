dac7de0ecf5c75622ba9b19e15b5d8e571f2750f
Sophia Chang <schang7jcu@gmail.com>
2025-03-13 19:50:50 -0700

    good job nathan cypress testing added

--- packages/frontend/cypress/e2e/firstTest.cy.js
+++ packages/frontend/cypress/e2e/firstTest.cy.js
@@ -1,6 +1,7 @@
 describe('Login Test', () => {
   it('Logs in successfully', () => {
-    cy.visit('http://localhost:5173/login');
+    //cy.visit('http://localhost:5173/login');
+    cy.visit('https://happy-sea-0b99f111e.6.azurestaticapps.net/login');
 
     cy.get('input[data-testid = "email-input"]').type('nluong03@calpoly.edu');
     cy.get('input[data-testid = "password-input"]').type("marquee");
